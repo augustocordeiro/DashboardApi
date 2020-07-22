43948596b31f89ea72ac7a2833b4c76279e1256b
Augusto Cordeiro Correa Pinto <augustoccp@gmail.com>
2020-07-22 10:26:35 -0300

    cliente com o  net 4

---
+++ src/DashboardClienteApiNet4/ConfiguracaoApi.cs
@@ -0,0 +1,8 @@
+﻿namespace DashboardClienteApiNet4
+{
+    public class ConfiguracaoApi
+    {
+        public string ChaveSecreta { get; set; }
+        public string ParametroAssinatura { get; set; }
+    }
+}
