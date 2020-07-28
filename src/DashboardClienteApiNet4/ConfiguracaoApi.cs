c14ac7c4c77283d523f045064c3d73dd120eb0cb
Augusto Cordeiro Correa Pinto <augustoccp@gmail.com>
2020-07-28 10:26:15 -0300

    ajustes solution

--- src/DashboardClienteApiNet4/ConfiguracaoApi.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DashboardClienteApiNet4
-{
-    public class ConfiguracaoApi
-    {
-        public string ChaveSecreta { get; set; }
-        public string ParametroAssinatura { get; set; }
-    }
-}
--- src/DashboardWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DashboardWeb.MvcApplication" Language="C#" %>
